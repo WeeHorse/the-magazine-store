980db976b27546769abe3a151333008b31d127d8
WeeHorse <benjaminberglund@gmail.com>
2018-05-18 12:53:31 +0200

    now with frond end vue starting point

--- server/server.js
+++ server/server.js
@@ -8,28 +8,40 @@ app.use(bodyParser.json());
 // MODELS
 const Product = require('./models/product.js');
 
-
 // ROUTES
-app.get('/products', async(req, res)=>{
+
+// serve frontend files
+app.use(express.static( '../client/'));
+
+// REST routes (backend routes)
+
+app.get('/rest/products', async(req, res)=>{
   //res.send('We are products');
   let products = await Product.find(); // {name:"The Times"}
   res.json(products);
 });
 
-app.post('/products', async(req, res)=>{
+app.get('/rest/products/:id', async(req, res)=>{
+  //res.send('We would get one product');
+  // get the product from the db
+  let product = await Product.findOne({_id: req.params.id});
+  res.json(product);
+});
+
+app.post('/rest/products', async(req, res)=>{
   //res.send('We would create a product');
   let product = await new Product(req.body);
   try{
     product.save();
-  }catch(e){
-    console.error(e);
+    res.json(product);
+  }catch(err){
+    console.error(err);
+    res.json(err);
   }
-  res.json(product);
 });
 
-app.put('/products/:id', async(req, res)=>{
+app.put('/rest/products/:id', async(req, res)=>{
   //res.send('We would update a product');
-
   // get the product from the db
   let product = await Product.findOne({_id: req.params.id});
   // perform update
@@ -40,8 +52,11 @@ app.put('/products/:id', async(req, res)=>{
   res.json(result);
 });
 
-app.delete('/products', (req, res)=>{
-  res.send('We would delete a product');
+app.delete('/rest/products/:id', async (req, res)=>{
+  //res.send('We would delete a product');
+  // delete the product from the db
+  let result = await Product.deleteOne({_id: req.params.id});
+  res.json(result);
 });
 
 app.listen('3000', ()=>{
